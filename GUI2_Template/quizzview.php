ea73323496571c9286c5ba27cbeded49698d5574
AlexandreJunod <36194606+AlexandreJunod@users.noreply.github.com>
2019-01-14 16:43:38 +0100

    Add template

---
+++ GUI2_Template/quizzview.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Quizzview_Template</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>QuizzView</h1>
+    <h3><a href="index.php">Home</a></h3>
+</body>
+</html>
