9aa66b4d55cb7de361f12c71853d56f80d534503
AlexandreJunod <36194606+AlexandreJunod@users.noreply.github.com>
2019-01-30 09:09:15 +0100

    Move questions

--- cleverquizz/src/router/index.js
+++ cleverquizz/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Questions from '@/components/Questions'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+    path: '/',
+    name: 'Questions',
+    component: Questions
     }
   ]
 })
