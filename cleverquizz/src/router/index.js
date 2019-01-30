b2a199932c8d9f289f20ffc825d945f560870ef8
Muminovic Anel <Muminovic____@hotmail.ch>
2019-01-30 09:48:01 +0100

    Install Bootstrap - Add route

--- cleverquizz/src/router/index.js
+++ cleverquizz/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Questions from '@/components/Questions'
 
 Vue.use(Router)
@@ -8,14 +7,9 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
     path: '/',
     name: 'Questions',
     component: Questions
-    }
+    },
   ]
 })
