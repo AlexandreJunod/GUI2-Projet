ee028eed0ee8cbdd52245720fbab134f13a92bb4
AlexandreJunod <36194606+AlexandreJunod@users.noreply.github.com>
2019-02-01 22:27:16 +0100

    Add template on master

---
+++ GUI2_Template/quizzselect.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Quizzselect_Template</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="logo"><a href="index.php"><img src="images/Logo.svg"></a></div>
+    <div class="title">Connexion</div>
+        <div class="row">
+            <div class="quizzitem col-sm-12 col-md-6 col-lg-3">
+                <div class="circle"></div>
+                <div class="labelitem">Maths</div>
+                <div class="progressbar">
+                    <div class="progressbarresult" style='width:50%'></div>
+                    <div class="progressbarlabel">50%</div>
+                </div>
+            </div>
+            <div class="quizzitem col-sm-12 col-md-6 col-lg-3">
+                <div class="circle"></div>
+                <div class="labelitem">Physique</div>
+                <div class="progressbar">
+                    <div class="progressbarresult" style='width:37%'></div>
+                    <div class="progressbarlabel">37%</div>
+                </div>
+            </div>
+            <div class="quizzitem col-sm-12 col-md-6 col-lg-3">
+                <div class="circle"></div>
+                <div class="labelitem">Science</div>
+                <div class="progressbar">
+                    <div class="progressbarresult" style='width:71%'></div>
+                    <div class="progressbarlabel">71%</div>
+                </div>
+            </div>
+            <div class="quizzitem col-sm-12 col-md-6 col-lg-3">
+                <div class="circle"></div>
+                <div class="labelitem">Geometrie</div>
+                <div class="progressbar">
+                    <div class="progressbarresult" style='width:13%'></div>
+                    <div class="progressbarlabel">13%</div>
+                </div>
+            </div>
+            <div class="quizzitem col-sm-12 col-md-6 col-lg-3">
+                <div class="circle"></div>
+                <div class="labelitem">Geometrie</div>
+                <div class="progressbar">
+                    <div class="progressbarresult" style='width:13%'></div>
+                    <div class="progressbarlabel">13%</div>
+                </div>
+            </div>
+            <div class="quizzitem col-sm-12 col-md-6 col-lg-3">
+                <div class="circle"></div>
+                <div class="labelitem">Geometrie</div>
+                <div class="progressbar">
+                    <div class="progressbarresult" style='width:13%'></div>
+                    <div class="progressbarlabel">13%</div>
+                </div>
+            </div>
+            <div class="quizzitem col-sm-12 col-md-6 col-lg-3">
+                <div class="circle"></div>
+                <div class="labelitem">Geometrie</div>
+                <div class="progressbar">
+                    <div class="progressbarresult" style='width:13%'></div>
+                    <div class="progressbarlabel">13%</div>
+                </div>
+            </div>
+            <div class="quizzitem col-sm-12 col-md-6 col-lg-3">
+                <div class="circle"></div>
+                <div class="labelitem">Geometrie</div>
+                <div class="progressbar">
+                    <div class="progressbarresult" style='width:13%'></div>
+                    <div class="progressbarlabel">13%</div>
+                </div>
+            </div>
+            <div class="quizzitem col-sm-12 col-md-6 col-lg-3">
+                <div class="circle"></div>
+                <div class="labelitem">Geometrie</div>
+                <div class="progressbar">
+                    <div class="progressbarresult" style='width:13%'></div>
+                    <div class="progressbarlabel">13%</div>
+                </div>
+            </div>
+            <div class="quizzitem col-sm-12 col-md-6 col-lg-3">
+                <div class="circle"></div>
+                <div class="labelitem">Geometrie</div>
+                <div class="progressbar">
+                    <div class="progressbarresult" style='width:13%'></div>
+                    <div class="progressbarlabel">13%</div>
+                </div>
+            </div>
+        </div><br/>
+</body>
+</html>
---
+++ GUI2_Template/quizzview.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Quizzview_Template</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="logo"><a href="index.php"><img src="images/Logo.svg"></a></div>
+    <div class="title">Connexion</div>
+</body>
+</html>
